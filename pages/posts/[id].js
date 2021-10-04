8d3b6cbbe155a7863a44a1da3b3e63946f7594fb
Sam <sam@electricenjin.com>
2021-10-04 11:25:13 -0400

    pulls content

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,40 @@
+import { withSSRContext } from "aws-amplify";
+import { Post } from "../../src/models";
+import Markdown from "react-markdown";
 import { useRouter } from "next/router";
 
-const Post = () => {
+export default function PostComponent({ post }) {
   const router = useRouter();
-  const { id } = router.query;
-
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
   return (
-    <p>
-      <p href="/">Post id: {id} </p>
-    </p>
+    <div>
+      <Markdown>{post.content}</Markdown>
+    </div>
   );
-};
+}
 
-export default Post;
+export async function getStaticPaths(req) {
+  const { DataStore } = withSSRContext(req);
+  const posts = await DataStore.query(Post);
+  const paths = posts.map((post) => ({ params: { id: post.id } }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(req) {
+  const { DataStore } = withSSRContext(req);
+  const { params } = req;
+  const { id } = params;
+  const post = await DataStore.query(Post, id);
+
+  return {
+    props: {
+      post: JSON.parse(JSON.stringify(post)),
+    },
+    revalidate: 1,
+  };
+}
